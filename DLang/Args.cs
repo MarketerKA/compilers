eebb02dcd29069cd6b4ad34428f904f765b76ad7
Daniil Tiuftin <daniil_t04@mail.ru>
2024-10-13 11:46:28 +0300

    Implement Args class

---
+++ DLang/Args.cs
@@ -0,0 +1,28 @@
+﻿namespace DLang
+{
+
+    internal class ArgsError : Exception
+    {
+        public ArgsError(string message) : base(message) { }
+
+        public ArgsError(string message, Exception innerException) : base(message, innerException) { }
+    }
+
+    internal class Args
+    {
+        public readonly string ProgramName;
+        public readonly string InputFilePath;
+
+        public Args(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                throw new ArgumentException("Not enough arguments");
+            }
+
+            ProgramName = args[0];
+            InputFilePath = args[1];
+        }
+    }
+
+}
