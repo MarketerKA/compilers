beb654ce0702067da0cd766a93f63728070e95fb
Evgeniy Spiridonov <e.spiridonov@innopolis.university>
2024-10-15 01:35:47 +0300

    Add testing, Fix tuple lexing

---
+++ DLang.Tests/Tests.cs
@@ -0,0 +1,42 @@
+using DLang.Lexing;
+using DLang.Parsing; // Add if needed
+
+
+namespace DLang.Tests;
+public class Tests
+{
+    public class ParserTests
+    {
+        [Test]
+        public void Test_Parser_Success()
+        {
+            // Simulate reading from a valid input file
+            string input = "var tup := {a := 1, b := 2};";
+
+            // Create the necessary components: Lexer, Scanner, Parser
+            Lexer lexer = new Lexer(input);
+            Scanner scanner = new Scanner(lexer, true);
+            Parser parser = new Parser(scanner);
+
+            // Attempt to parse
+            bool result = parser.Parse();
+
+            // Check that parsing succeeds
+            Assert.IsTrue(result, "Parsing failure occurred when it should have succeeded.");
+        }
+
+        [Test]
+        public void Test_Parser_Failure()
+        {
+            string input = "var tup := {a := 1 b := 2};";
+
+            Lexer lexer = new Lexer(input);
+            Scanner scanner = new Scanner(lexer, true);
+            Parser parser = new Parser(scanner);
+
+            bool result = parser.Parse();
+
+            Assert.IsFalse(result, "Parsing succeeded when it should have failed.");
+        }
+    }
+}
--- DLang/Lexing/Lexer.cs
+++ DLang/Lexing/Lexer.cs
@@ -48,6 +48,7 @@ namespace DLang.Lexing
             string value = _input.Substring(start, _position - start);
             return new Token(GetKeywordType(value), value);
         }
+        
 
         private Tokens GetKeywordType(string value)
         {
@@ -84,31 +85,40 @@ namespace DLang.Lexing
         {
             int start = _position;
             bool isReal = false;
-            while (_currentChar != '\0')
-            {
-                if (char.IsDigit(_currentChar))
-                {
+            if (_input[_position - 1] == '.'){
+                while(_currentChar != '\0' && _currentChar != '.'){
                     Advance();
-                }
-                else if (_currentChar == '.')
-                {
-                    Advance();
-                    if (_currentChar == '.')
-                    {
-                        _position--;
-                        break;
-                    }
-                    isReal = true;
-                }
-                else if (char.IsLetter(_currentChar))
-                {
-                    throw new Exception($"Unexpected character after number: {_currentChar}");
-                }
-                else
-                {
                     break;
                 }
             }
+            else {
+                while (_currentChar != '\0')
+                {
+                    if (char.IsDigit(_currentChar))
+                    {
+                        Advance();
+                    }
+                    else if (_currentChar == '.')
+                    {
+                        Advance();
+                        if (_currentChar == '.')
+                        {
+                            _position--;
+                            break;
+                        }
+                        isReal = true;
+                    }
+                    else if (char.IsLetter(_currentChar))
+                    {
+                        throw new Exception($"Unexpected character after number: {_currentChar}");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            
             string value = _input.Substring(start, _position - start);
             return new Token(isReal ? Tokens.REAL_LITERAL : Tokens.INTEGER_LITERAL, value);
         }
