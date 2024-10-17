9780fd33786180b23dc82c1991d8b918b6c8b6ce
Daniil Tiuftin <daniil_t04@mail.ru>
2024-10-17 19:08:04 +0300

    Add Locationed class

---
+++ DLang/Parsing/Locationed.cs
@@ -0,0 +1,16 @@
+﻿using QUT.Gppg;
+
+namespace DLang.Parsing
+{
+
+    internal class Locationed
+    {
+        public LexLocation Location { get; set; }
+
+        public Locationed(LexLocation location)
+        {
+            Location = location;
+        }
+    }
+
+}
