f2c4236c339875e17a64bc474fe04186d4360694
Daniil Tiuftin <daniil_t04@mail.ru>
2024-10-18 00:59:55 +0300

    Add JsonIgnore to Locationed.Location

--- DLang/Parsing/Locationed.cs
+++ DLang/Parsing/Locationed.cs
@@ -1,10 +1,12 @@
 ﻿using QUT.Gppg;
+using System.Text.Json.Serialization;
 
 namespace DLang.Parsing
 {
 
     internal class Locationed
     {
+        [JsonIgnore]
         public LexLocation Location { get; set; }
 
         public Locationed(LexLocation location)
