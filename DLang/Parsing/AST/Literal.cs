f62b24fb78cafd00f30e81584a72d9aa135d3359
Daniil Tiuftin <daniil_t04@mail.ru>
2024-10-17 13:10:00 +0300

    Assign type to literals

--- DLang/Parsing/AST/Literal.cs
+++ DLang/Parsing/AST/Literal.cs
@@ -1,8 +1,20 @@
 ﻿namespace DLang.Parsing.AST
 {
 
+    internal enum LiteralType
+    {
+        Int,
+        Double,
+        Bool,
+        String,
+        Tuple,
+        Array,
+        Empty
+    }
+
     internal class Literal
     {
+        public readonly LiteralType Type;
         public readonly Int128? IntValue;
         public readonly double? DoubleValue;
         public readonly bool? BoolValue;
@@ -10,19 +22,39 @@
         public readonly Tuple? TupleValue;
         public readonly Array? ArrayValue;
 
-        public Literal(Int128 value) { IntValue = value; }
+        public Literal(Int128 value) { 
+            IntValue = value; 
+            Type = LiteralType.Int;
+        }
 
-        public Literal(double value) { DoubleValue = value; }
+        public Literal(double value) { 
+            DoubleValue = value;
+            Type = LiteralType.Double;
+        }
 
-        public Literal(bool value) { BoolValue = value; }
+        public Literal(bool value) { 
+            BoolValue = value; 
+            Type = LiteralType.Bool;
+        }
 
-        public Literal(string value) { StringValue = value; }
+        public Literal(string value) { 
+            StringValue = value;
+            Type = LiteralType.String;
+        }
 
-        public Literal(Tuple value) { TupleValue = value; }
+        public Literal(Tuple value) { 
+            TupleValue = value; 
+            Type = LiteralType.Tuple;
+        }
 
-        public Literal(Array value) { ArrayValue = value; }
+        public Literal(Array value) { 
+            ArrayValue = value; 
+            Type = LiteralType.Array;
+        }
 
-        public Literal() { }
+        public Literal() {
+            Type = LiteralType.Empty;
+        }
     }
 
 }
