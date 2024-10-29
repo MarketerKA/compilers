7336c17d2b1102839defb405c5c82685c2255562
Daniil Tiuftin <daniil_t04@mail.ru>
2024-10-29 16:50:12 +0300

    Catch ConstexprValue errors

---
+++ DLang/Analysis/OptimizerError.cs
@@ -0,0 +1,21 @@
+﻿using QUT.Gppg;
+
+namespace DLang.Analysis
+{
+
+    internal class OptimizerError : Exception
+    {
+        public readonly LexLocation Location;
+
+        public OptimizerError(LexLocation location, string message) : base(message)
+        {
+            Location = location;
+        }
+
+        public OptimizerError(LexLocation location, string message, Exception innerException) : base(message, innerException)
+        {
+            Location = location;
+        }
+    }
+
+}
