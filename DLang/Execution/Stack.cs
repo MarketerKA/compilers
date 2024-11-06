429e26396c531524efd858c29d6547c4c0ff3144
Daniil Tiuftin <daniil_t04@mail.ru>
2024-11-07 02:07:48 +0300

    Implement Stack

---
+++ DLang/Execution/Stack.cs
@@ -0,0 +1,59 @@
+﻿namespace DLang.Execution
+{
+
+    internal class Stack
+    {
+        public readonly List<Namespace> Namespaces;
+        public readonly int MaxDepth;
+
+        public Stack(int maxDepth)
+        {
+            if (maxDepth <= 0)
+            {
+                throw new ArgumentException("negative staick maxDepth");
+            }
+
+            Namespaces = [];
+            MaxDepth = maxDepth;
+        }
+
+        public void Push()
+        {
+            if (Namespaces.Count + 1 > MaxDepth)
+            {
+                throw new StackOverflowError($"stack overflow (max depth: {MaxDepth})");
+            }
+
+            Namespaces.Add(new());
+        }
+
+        public void Pop()
+        {
+            Namespaces.RemoveAt(Namespaces.Count - 1);
+        }
+
+        public Namespace Peek()
+        {
+            return Namespaces[^1];
+        }
+
+        public Value GetValue(string name)
+        {
+            foreach (Namespace ns in Namespaces.AsEnumerable().Reverse())
+            {
+                if (ns.Has(name))
+                {
+                    return ns.Get(name);
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        public void PutValue(string name, Value value)
+        {
+            Namespaces[^1].Put(name, value);
+        }
+    }
+
+}
