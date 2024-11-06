de56e0602854e8ef1cc7f23da984bd498d161ba1
Daniil Tiuftin <daniil_t04@mail.ru>
2024-11-07 02:05:14 +0300

    Add support for escaped strings

---
+++ DLang/Lexing/LexerError.cs
@@ -0,0 +1,17 @@
+﻿using QUT.Gppg;
+
+namespace DLang.Lexing
+{
+
+    internal class LexerError : Exception
+    {
+        public readonly LexLocation Location;
+
+        public LexerError(LexLocation location, string msg) : base(msg) 
+        {
+            Location = location;
+        }
+
+    }
+
+}
