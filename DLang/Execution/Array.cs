800ea234a093577b05c6142d7478e1c0788c6d51
Daniil Tiuftin <daniil_t04@mail.ru>
2024-11-07 11:56:38 +0300

    Always print array indices in ToString()

--- DLang/Execution/Array.cs
+++ DLang/Execution/Array.cs
@@ -61,10 +61,7 @@ namespace DLang.Execution
             int i = 0;
             foreach (var pair in Values)
             {
-                if (!_contiguous)
-                    sb.Append($"{pair.Key}: {pair.Value}");
-                else
-                    sb.Append(pair.Value.ToString());
+                sb.Append($"[{pair.Key}] = {pair.Value}");
 
                 if (i < Values.Count - 1) sb.Append(", ");
                 i++;
